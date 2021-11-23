5c619f809732d2769add3664ddc476bb7282dfd9
Shiboko Token <93381759+Shiboko@users.noreply.github.com>
2021-11-24 00:47:11 +0300

    Add files via upload

---
+++ NFT.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.7;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract NFT is ERC721 {
+  constructor() ERC721('My NFT', 'NFT') {
+    _mint(msg.sender, 0);
+    _mint(msg.sender, 1);
+  }
+}
